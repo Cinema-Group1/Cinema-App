7932e9cd011bea30dd2f4b8a67d6bc5f9f336878
justSchwab <justin.schwab@t-online.de>
2023-02-10 02:40:37 +0100

    final

--- app/src/main/java/com/example/qrscanner/MainActivity.java
+++ app/src/main/java/com/example/qrscanner/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.qrscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.material.button.MaterialButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,33 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        TextView username =(TextView) findViewById(R.id.username);
-        TextView password =(TextView) findViewById(R.id.password);
-
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
         MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
 
-        //admin and admin
 
+        // login button
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(username.getText().toString().equals("sebo") && password.getText().toString().equals("snitch")){
+                // sebo snitch as user
+                if (username.getText().toString().equals("sebo") && password.getText().toString().equals("snitch")) {
                     //scan
                     startActivity(new Intent(MainActivity.this, ScanActivity.class));
-                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
-                }
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
-                    //admin
-                    startActivity(new Intent(MainActivity.this, AdminActivity.class));
                     Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-                }
-                else
-                    //incorrect
-                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
+                } else
+                    //incorrect user name
+                    Toast.makeText(MainActivity.this, "LOGIN FAILED !!!", Toast.LENGTH_SHORT).show();
             }
         });
-
 
     }
 }
--- app/src/main/java/com/example/qrscanner/ScanActivity.java
+++ app/src/main/java/com/example/qrscanner/ScanActivity.java
@@ -3,19 +3,21 @@ package com.example.qrscanner;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
+
+
 
 public class ScanActivity extends AppCompatActivity {
 
     Button button_scan;
     Button button_logout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +34,14 @@ public class ScanActivity extends AppCompatActivity {
         });
     }
 
+    // logout button
     private void logout(){
         startActivity(new Intent(ScanActivity.this, MainActivity.class));
     }
 
+    // QR Code scannen
     private void scanCode(){
+        // Berechtigungen werden abgefragt
         ScanOptions config = new ScanOptions();
         config.setPrompt("Volume up to flash on");
         config.setBeepEnabled(true);
@@ -46,16 +51,26 @@ public class ScanActivity extends AppCompatActivity {
 
     }
     ActivityResultLauncher<ScanOptions> barLaucher = registerForActivityResult(new ScanContract(), result -> {
-        if(result.getContents() != null){
+        if(result.getContents() != null) {
             AlertDialog.Builder builder = new AlertDialog.Builder(ScanActivity.this);
             builder.setTitle("Result");
-            builder.setMessage(result.getContents());
+            String content = result.getContents();
+            // Prüfung, ob QR Code in Rep liegt
+            if (content.startsWith("https://cinema-backend-group1.azurewebsites.net/ticket/check:")){
+                builder.setMessage("Scan war erfolgreich!");
+            }else{
+                // QR Code stellt kein valides Ticket dar
+                builder.setMessage("QR Code ist ungültig!");
+            }
             builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int i) {
                     dialogInterface.dismiss();
                 }
             }).show();
-        }
+
+            }
     });
+
+
 }
